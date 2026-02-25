e2fa19f05109a5c9e20652e125cf7f0696287408
Technicbuilder <151921436+Technicbuilder@users.noreply.github.com>
2026-02-25 15:06:11 +0000

    Refactor piece generation and fix king assignment
    Cleaned up logic, made it look simpler and removed repeat code

--- board.py
+++ board.py
@@ -10,30 +10,30 @@ class Board:
         self.board = [[0 for x in range(8)] for x in range(8)]
         self.white_pieces = []
         self.black_pieces = []
+        self.generate_pieces()
+
+    def generate_pieces(self):
         #   generate rooks
-        self.board[0][0] = Rook(starting_position=(0, 0), colour=True)
-        self.board[0][7] = Rook(starting_position=(7, 0), colour=True)
-        self.board[7][0] = Rook(starting_position=(0, 7), colour=False)
-        self.board[7][7] = Rook(starting_position=(7, 7), colour=False)
+        rook_starting_positions = [(0, 0, True), (0, 7, True), (7, 0, False), (7, 7, False)]
+        self.assign_object(piece=Rook, attributes=rook_starting_positions)
+
         #   generate knights
-        self.board[0][1] = Knight(starting_position=(1, 0), colour=True)
-        self.board[0][6] = Knight(starting_position=(6, 0), colour=True)
-        self.board[7][1] = Knight(starting_position=(1, 7), colour=False)
-        self.board[7][6] = Knight(starting_position=(6, 7), colour=False)
+        knight_starting_positions = [(0, 1, True), (0, 6, True), (7, 1, False), (7, 6, False)]
+        self.assign_object(piece=Knight, attributes=knight_starting_positions)
         
         #   generate bishops
-        self.board[0][2] = Bishop(starting_position=(2, 0), colour=True)
-        self.board[0][5] = Bishop(starting_position=(5, 0), colour=True)
-        self.board[7][2] = Bishop(starting_position=(2, 7), colour=False)
-        self.board[7][5] = Bishop(starting_position=(5, 7), colour=False)
+        bishop_starting_positions = [(0, 2, True), (0, 5, True), (7, 2, False), (7, 5, False)]
+        self.assign_object(piece=Bishop, attributes=bishop_starting_positions)
+        
         
         #   generate queens
-        self.board[0][3] = Queen(starting_position=(3, 0), colour=True)
-        self.board[7][3] = Queen(starting_position=(3, 7), colour=False)
+        queen_starting_positions = [(0, 3, True), (7, 3, False)]
+
+        self.assign_object(piece=Queen, attributes=queen_starting_positions)
         
         #   generate kings
-        self.board[0][4] = King(starting_position=(4, 0), colour=True)
-        self.board[7][4] = King(starting_position=(4, 7), colour=False) 
+        king_starting_positions = [(0, 4, True), (7, 4, False)]
+        self.assign_object(piece=Queen, attributes=king_starting_positions)
         
         #   generate pawns
         for spaces in range(8):
@@ -42,22 +42,26 @@ class Board:
             
         for rows in self.board:
             for pieces in rows:
-                
                 if pieces == 0:
                     continue
                 colour = getattr(pieces, 'colour', False)
-                
+
                 if colour:
                     self.white_pieces.append(pieces)
                 else:
                     self.black_pieces.append(pieces)
             
         
-        
+    def assign_object(self, piece, attributes):
+        for (y, x , colour) in attributes:
+            self.board[y][x] = piece(starting_position=(x, y), colour=colour)
+
+
     def print_board(self):
         print('      a    b    c    d    e    f    g    h')
+        print('===========================================')
         for i, rows in enumerate(self.board):
-            print(f'{i+1} ', ' '.join(f"{str(item):>4}" for item in rows))
+            print(f'{i+1}|', ' '.join(f"{str(item):>4}" for item in rows))
             
             
             
